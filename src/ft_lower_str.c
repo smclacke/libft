02bb7e248110a69a25f81938ed9760e3d92002c9
Sarah <115113929+smclacke@users.noreply.github.com>
2023-03-28 02:17:19 +0200

    Add files via upload

--- src/ft_lower_str.c
+++ src/ft_lower_str.c
@@ -6,7 +6,7 @@
 /*   By: SarahLouise <SarahLouise@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 22:17:06 by smclacke          #+#    #+#             */
-/*   Updated: 2023/03/28 02:06:03 by SarahLouise      ###   ########.fr       */
+/*   Updated: 2023/03/28 02:16:43 by SarahLouise      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 char	*ft_lower_str(char *str)
 {
-	char	*lowercase_str;
 	int		len;
 	int		i;
 
 	i = 0;
-	lowercase_str = ft_strdup(&str[i]);
-	len = ft_strlen(lowercase_str);
+    len = ft_strlen(str);
 	while (i < len)
 	{
-		lowercase_str[i] = ft_tolower(lowercase_str[i]);
+		str[i] = ft_tolower(str[i]);
 		i++;
 	}
-	return (lowercase_str);
+	return (str);
 }
