bf0002fd90c3d57d5e288fe75888ba5248cdb66d
Sarah Mclacken <smclacke@f0r3s13.codam.nl>
2023-04-18 17:06:17 +0200

    atoi in progress

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,22 +6,53 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/24 15:39:55 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/04/15 20:18:23 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/04/18 17:05:00 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// int	ft_atoi(const char *str)
+// {
+// 	int		neg;
+// 	long	num;
+// 	int		i;
+
+// 	i = 0;
+// 	neg = 1;
+// 	num = 0;
+// 	while (ft_isspace(str[i]))
+// 		i++;
+// 	if (str[i] == '-' || str[i] == '+')
+// 	{
+// 		if (str[i] == '-')
+// 			neg *= -1;
+// 		i++;
+// 	}
+// 	while (ft_isdigit(str[i]))
+// 	{
+// 		num = num * 10 + (str[i] - 48);
+// 		i++;
+// 	}
+// 	if ((num * neg) > INT_MAX || (num * neg) < INT_MIN)
+// 		write(1, "NOPE\n", 10);
+// 	return (num * neg);
+// }
+
+
+
 int	ft_atoi(const char *str)
 {
-	int		neg;
+	long	neg;
 	long	num;
-	int		i;
+	long	i;
+	long 	result;
 
 	i = 0;
 	neg = 1;
 	num = 0;
-	while (ft_isspace(str[i]))
+	result = 0;
+	while ((ft_isspace(str[i])))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
@@ -34,7 +65,12 @@ int	ft_atoi(const char *str)
 		num = num * 10 + (str[i] - 48);
 		i++;
 	}
-	if ((num * neg) > INT_MAX || (num * neg) < INT_MIN)
-		write(1, "NOPE\n", 10);
-	return (num * neg);
+	result += num * neg;
+	if (!(result > INT_MAX || result < INT_MIN))
+		return (num * neg);
+	else
+	{
+		printf("error atoi\n");
+		return (EXIT_FAILURE);
+	}
 }
