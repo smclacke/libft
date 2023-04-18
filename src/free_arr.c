61d352822deb33f86ad461070531f1bd98a71ddf
Sarah Mclacken <smclacke@f0r3s13.codam.nl>
2023-04-18 20:55:33 +0200

    normed

--- src/free_arr.c
+++ src/free_arr.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/18 20:53:30 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/04/18 20:53:43 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/04/18 20:55:23 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	free_arr(char **arr)
 	int	i;
 
 	i = 0;
-	while(arr[i])
+	while (arr[i])
 		free(arr[i++]);
 	free(arr);
 }
