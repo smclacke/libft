adbc172eea5da99579374c755505c73f4b8911d5
Sarah McLacken <SarahLouise@sarahs-mbp-2.lan>
2024-01-28 23:15:43 +0100

    nothing of interst

--- src/strs/ft_strlen.c
+++ src/strs/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/24 15:41:32 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/07/02 15:06:31 by SarahLouise   ########   odam.nl         */
+/*   Updated: 2024/01/28 23:15:26 by SarahLouise   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
